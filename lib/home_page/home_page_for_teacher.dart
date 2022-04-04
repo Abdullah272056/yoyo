9d03a210f7e1657530d43e8e9f4ca76d2e4a24f6
lenovo <Abdullah272056@gmail.com>
2022-04-04 12:32:20 +0600

    teachers all class room show by gridview and shimmer effect

--- lib/home_page/home_page_for_teacher.dart
+++ lib/home_page/home_page_for_teacher.dart
@@ -1,13 +1,16 @@
 
 import 'dart:convert';
+
 import 'dart:io';
 
+import 'package:aws_exam_portal/api%20service/NoDataFound.dart';
 import 'package:aws_exam_portal/api%20service/api_service.dart';
 import 'package:aws_exam_portal/api%20service/sharePreferenceDataSaveName.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeForTeacherScreen extends StatefulWidget {
   const HomeForTeacherScreen({Key? key}) : super(key: key);
@@ -26,7 +29,7 @@ class _HomeForTeacherScreenState extends State<HomeForTeacherScreen> {
   String _otpTxt = "";
   String _userId = "";
   bool shimmerStatus=true;
-  List teacherRoomList=[];
+  List teacherClassRoomList=[];
   var teacherRoomListResponse;
 
   @override
@@ -46,28 +49,198 @@ class _HomeForTeacherScreenState extends State<HomeForTeacherScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.backGroundColor,
-        body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ).copyWith(top: 20),
-              child: Column(
-                children: [
-                  Text("Home Page for teacher"),
-                ],
-              ),
-            ),
-          )
+        body: RefreshIndicator(
+          color: Colors.white,
+          backgroundColor: Colors.purple,
+          triggerMode: RefreshIndicatorTriggerMode.anywhere,
+          onRefresh: () async {
+            await Future.delayed(const Duration(seconds: 1));
 
-          ,
+            updateDataAfterRefresh();
+          },
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+
+              if(shimmerStatus)...[
+                Expanded(child: Flex(
+                  direction: Axis.vertical,
+                  children: [
+                    SizedBox(
+                      height: 8,
+                    ),
+                    _buildExploreCityListShimmer(),
+                  ],
+                ))
+
+
+              ]else...[
+                if (teacherClassRoomList != null && teacherClassRoomList.length > 0) ...[
+                  Expanded(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                        ).copyWith(top: 5, bottom: 10),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Expanded(
+                              child: _buildTeacherClassRoomList(),
+                              flex: 1,
+                            ),
+                          ],
+                        )),
+                  )
+                ] else ...[
+                  Expanded(
+                    child:NoDataFound().noItemFound("NO ROOM FOUND!"),
+                  ),
+
+                ],
+              ]
+
+            ],
+          ),
+
         ),
       ),
     );
   }
 
+  Widget _buildTeacherClassRoomList() {
+    return GridView.builder(
+        itemCount: teacherClassRoomList == null ? 0 : teacherClassRoomList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 2.0,
+          mainAxisSpacing: 2.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return InkResponse(
+            child: Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(7.0),
+                child: Container(
+                  width: 160,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Expanded(child:Center(
+                        child:  Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("Class Room Name",style: TextStyle(color: Colors.awsEndColor,fontSize: 16,),),
+                            Text(teacherClassRoomList[index]["class_room_name"].toString(),
+                                style: TextStyle(color: Colors.awsEndColor,fontSize: 20,fontWeight: FontWeight.w700)),
+                          ],
+                        ),),),
+                      Expanded(child:Center(
+                        child:  Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("Class Room Code",style: TextStyle(color: Colors.awsEndColor,fontSize: 16,),),
+                            Text(teacherClassRoomList[index]["class_room_code"].toString(),
+                                style: TextStyle(color: Colors.awsEndColor,fontSize: 20,fontWeight: FontWeight.w700)),
+                          ],
+                        ),),),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            onTap: () {
+              //_showToast("Clicked Item $index");
+
+
+              // Navigator.push(
+              //     context,
+              //     MaterialPageRoute(
+              //         builder: (context) => RoomDetailsScreen(
+              //             offerDataList[index]["room_id"].toString())));
+            },
+          );
+        });
+  }
+
+
+  Widget _buildExploreCityListShimmer(){
+    return Expanded(
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+
+            return ListView(
+              children: [
+                SizedBox(
+                  height: constraints.maxHeight,
+                  child:Padding(
+                    padding: const EdgeInsets.only(left: 5,right: 5,top: 5),
+                    child: GridView.builder(
+                        itemCount: 16,
+                        physics: const NeverScrollableScrollPhysics(),
+                        // physics: const AlwaysScrollableScrollPhysics(),
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 0.0,
+                          mainAxisSpacing: 0.0,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            elevation: 1,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(7.0),
+                              child: Container(
+                                width: 160,
+                                color: Colors.white,
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: Stack(children: <Widget>[
+                                        Shimmer.fromColors(
+                                          baseColor: Colors.shimmer_baseColor,
+                                          highlightColor:
+                                          Colors.shimmer_highlightColor,
+                                          child: Container(
+                                            height: double.infinity,
+                                            width: double.infinity,
+                                            color: Colors.black.withOpacity(0.3),
+                                          ),
+                                        ),
+
+
+                                      ]),
+                                      flex: 1,
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+
+                        }),),
+                ),
+
+              ],
+            );
+          }),
+
+
+      flex: 1,
+    );
+  }
 
   _getTeacherRoomDataList(String u_id) async {
     try {
@@ -84,8 +257,8 @@ class _HomeForTeacherScreenState extends State<HomeForTeacherScreen> {
             setState(() {
               //_showToast("success");
               var data = jsonDecode(response.body);
-              teacherRoomList = data["data"];
-              _showToast(teacherRoomList.length.toString());
+              teacherClassRoomList = data["data"];
+              //_showToast(teacherClassRoomList.length.toString());
               shimmerStatus=false;
             });
           } else {
@@ -114,126 +287,9 @@ class _HomeForTeacherScreenState extends State<HomeForTeacherScreen> {
         textColor: Colors.white,
         fontSize: 16.0);
   }
-
-  // void _showAlertDialog(BuildContext context){
-  //   showDialog(
-  //     context: context,
-  //     builder: (context) {
-  //       // return VerificationScreen();
-  //       return Dialog(
-  //         child: Wrap(
-  //           children: [
-  //             Container(
-  //               margin: const EdgeInsets.only(
-  //                   left: 15.0, right: 15.0, top: 20, bottom: 20),
-  //               child: Column(
-  //                 children: [
-  //                   Padding(
-  //                     padding: const EdgeInsets.symmetric(
-  //                       horizontal: 30,
-  //                     ),
-  //                     child: Column(
-  //                       children: [
-  //                         Image.asset(
-  //                           "assets/images/porzotok.png",
-  //                           width: 120,
-  //                           height: 65,
-  //                         )
-  //                       ],
-  //                     ),
-  //                   ),
-  //                   // Image.asset('assets/images/profile.jpg'),
-  //                   const Text(
-  //                     "let's Discover Bangladesh Together",
-  //                     style: TextStyle(
-  //                       fontFamily: 'PT-Sans',
-  //                       fontSize: 14,
-  //                       fontWeight: FontWeight.normal,
-  //                       color: Colors.black,
-  //                     ),
-  //                   ),
-  //                   // Image.asset('assets/images/profile.jpg'),
-  //                   const SizedBox(
-  //                     height: 30,
-  //                   ),
-  //                   const Text(
-  //                     "Verification code has been send to your mobile",
-  //                     style: TextStyle(
-  //                       fontFamily: 'PT-Sans',
-  //                       fontSize: 12,
-  //                       fontWeight: FontWeight.normal,
-  //                       color: Colors.black54,
-  //                     ),
-  //                   ),
-  //                   const SizedBox(
-  //                     height: 30,
-  //                   ),
-  //
-  //                   //phone number input
-  //                   _buildTextFieldOTPView(
-  //                     hintText: 'Enter 6 digit Number',
-  //                     obscureText: false,
-  //                     prefixedIcon:
-  //                         const Icon(Icons.phone, color: Colors.appRed),
-  //                   ),
-  //                   const SizedBox(
-  //                     height: 30,
-  //                   ),
-  //
-  //                   _buildVerifyButton(),
-  //
-  //                   const SizedBox(
-  //                     height: 20,
-  //                   ),
-  //                   _buildVerifyQuestion(),
-  //                 ],
-  //               ),
-  //             )
-  //           ],
-  //           // child: VerificationScreen(),
-  //         ),
-  //       );
-  //     },
-  //   );
-  // }
-
-  void _showLoadingDialog(BuildContext context){
-    showDialog(
-      context: context,
-      builder: (context) {
-        // return VerificationScreen();
-        return Dialog(
-          child: Wrap(
-            children: [
-              Container(
-                  margin: const EdgeInsets.only(
-                      left: 15.0, right: 15.0, top: 30, bottom: 30),
-                  child: Center(
-                    child: Row(
-                      children: const [
-                        SizedBox(
-                          width: 10,
-                        ),
-                        CircularProgressIndicator(
-                          backgroundColor: Colors.appRed,
-                          strokeWidth: 5,
-                        ),
-                        SizedBox(
-                          width: 12,
-                        ),
-                        Text(
-                          "Checking...",
-                          style: TextStyle(fontSize: 25),
-                        )
-                      ],
-                    ),
-                  ))
-            ],
-            // child: VerificationScreen(),
-          ),
-        );
-      },
-    );
+  void updateDataAfterRefresh() {
+    _getTeacherRoomDataList(_userId);
+    setState(() {});
   }
 
   loadUserIdFromSharePref() async {
